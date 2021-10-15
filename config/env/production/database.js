6e2952595d91104c0a9b5afd8c9058729b9735eb
tahafatimaDO <82136225+tahafatimaDO@users.noreply.github.com>
2021-10-15 08:38:23 +0530

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,15 @@
-const parse = require('pg-connection-string').parse
-const config = parse(process.env.DATABASE_URL)
-
-module.exports = () => ({
+module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', ';'),
         ssl: {
           rejectUnauthorized: false,
         },
@@ -20,4 +17,4 @@ module.exports = () => ({
       options: {},
     },
   },
-})
+});
